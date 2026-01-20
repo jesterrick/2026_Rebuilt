4dac2faeb3996afc61f4eba02231bf0088b585e4
Rick Jester <rickjester@gmail.com>
2026-01-20 18:51:22 -0500

    framework

---
+++ src/main/java/frc/robot/constants/FeederConstants.java
@@ -0,0 +1,14 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.constants;
+
+/** Add your docs here. */
+public class FeederConstants {
+
+    public static final int kFeederMotor = 9;
+    public static final double kFeederMotorStop = 0.0;
+    public static final double kFeederSpeed = 0.5;
+}
+
