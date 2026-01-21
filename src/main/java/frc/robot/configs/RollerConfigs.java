375cffd930c0f0b2916d9f550b0e8b819f491664
sy1vee <drid124509@gmail.com>
2026-01-20 19:54:58 -0500

    Config and Commands
    Config and commands created. Roller on/off created.

--- src/main/java/frc/robot/configs/RollerConfigs.java
+++ src/main/java/frc/robot/configs/RollerConfigs.java
@@ -1,8 +1,18 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.configs;
 
-/** Add your docs here. */
-public class RollerConfigs {}
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import frc.robot.constants.GlobalConstants;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+public class RollerConfigs {
+    public static final SparkMaxConfig config = new SparkMaxConfig();
+
+    static{
+        config.inverted(false);
+        config.idleMode(IdleMode.kBrake);
+        config.smartCurrentLimit(GlobalConstants.kLowCurrentLimit);
+    }
+
+}
+
+
