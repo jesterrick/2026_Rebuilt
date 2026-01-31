48bec90d63bcc6479afc3d6d81d76f3e17b37945
Rick Jester <rickjester@gmail.com>
2026-01-31 17:27:08 -0500

    feeder on when launcher at correct speed

--- src/main/java/frc/robot/subsystems/LauncherSubsystem.java
+++ src/main/java/frc/robot/subsystems/LauncherSubsystem.java
@@ -11,37 +11,64 @@ import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.configs.LauncherConfigs;
 import frc.robot.constants.CanIdConstants;
+import frc.robot.constants.LauncherConstants;
 
 public class LauncherSubsystem extends SubsystemBase {
   /** Creates a new LauncherSubsystem. */
   private final SparkMax m_LauncherMotor;
   private final SparkClosedLoopController m_ClosedLoopController;
+  private double targetRPM;
 
   public LauncherSubsystem() {
     this.m_LauncherMotor = new SparkMax(CanIdConstants.kLauncherMotor, MotorType.kBrushless);
     this.m_ClosedLoopController = m_LauncherMotor.getClosedLoopController();
     this.m_LauncherMotor.configure(LauncherConfigs.config, ResetMode.kResetSafeParameters,
         PersistMode.kPersistParameters);
+    this.targetRPM = 0.0;
+  }
+
+  @Override
+  public void periodic() {
+    // Only use the PID controller if we have a target speed.
+    // Otherwise, let the motor be still or coast.
+    if (this.targetRPM > 0) {
+      this.m_ClosedLoopController.setSetpoint(this.targetRPM, ControlType.kVelocity);
+    } else {
+      this.m_LauncherMotor.stopMotor();
+    }
+
+    // This sends two lines to a graph so you can see them overlap
+    SmartDashboard.putNumber("Launcher/Target RPM", this.targetRPM);
+    SmartDashboard.putNumber("Launcher/Actual RPM", this.getActualVelocity());
+    
+    // True if spinning and within tolerance; False if bogged down or stopped
+    SmartDashboard.putBoolean("Launcher/At Speed", this.atSpeed());
   }
 
   /**
    * Sets the launcher to a specific velocity.
    * 
-   * @param targetRPM The desired speed (e.g., calculated from vision distance)
+   * @param setRPM The desired speed (e.g., calculated from vision distance)
    */
-  public void setLauncherVelocity(double targetRPM) {
-    m_ClosedLoopController.setSetpoint(targetRPM, ControlType.kVelocity);
+  public void setLauncherVelocity(double setRPM) {
+    this.targetRPM = setRPM;
   }
 
-  public double getActualVelocity(){
+  public double getActualVelocity() {
     return this.m_LauncherMotor.getEncoder().getVelocity();
   }
 
   public void stopLauncher() {
+    this.targetRPM = 0.0;
     this.m_LauncherMotor.stopMotor();
   }
 
+  public boolean atSpeed() {
+    return (this.targetRPM > 0)
+        && (Math.abs(this.targetRPM - getActualVelocity()) < LauncherConstants.kLauncherTolerance);
+  }
 }
