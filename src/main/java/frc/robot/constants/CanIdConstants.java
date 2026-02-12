e3865b58c3c02487ec2ce096ac91d6d5a03f2c68
Rick Jester <rickjester@gmail.com>
2026-02-12 18:58:32 -0500

    fix can ids

--- src/main/java/frc/robot/constants/CanIdConstants.java
+++ src/main/java/frc/robot/constants/CanIdConstants.java
@@ -4,17 +4,31 @@ package frc.robot.constants;
  * The CanIdConstants class centralizes all CAN IDs for the robot's motors,
  * ensuring easy management and modification of hardware addresses.
  */
+
+/*
+ * CAN ID RANGES
+ * Turning 1 - 4
+ * Driving 5 - 8
+ * Intake 10 - 14
+ * Rollers 15 - 19
+ * Extender 20 - 24
+ * Climber 25-29
+ * Feeder 30 - 34
+ * Launcher 35-39
+ * PDH - 60
+ */
+
 public class CanIdConstants {
     // CAN IDs for the Swerve Modules
-    public static final int kFrontLeftTurningCanId = 1;
-    public static final int kRearLeftTurningCanId = 3;
-    public static final int kFrontRightTurningCanId = 7;
-    public static final int kRearRightTurningCanId = 5;
+    public static final int kFrontLeftTurningCanId = 5;
+    public static final int kRearLeftTurningCanId = 7;
+    public static final int kFrontRightTurningCanId = 3;
+    public static final int kRearRightTurningCanId = 1;
 
-    public static final int kFrontLeftDrivingCanId = 2;
-    public static final int kRearLeftDrivingCanId = 4;
-    public static final int kFrontRightDrivingCanId = 8;
-    public static final int kRearRightDrivingCanId = 6;
+    public static final int kFrontLeftDrivingCanId = 6;
+    public static final int kRearLeftDrivingCanId = 8;
+    public static final int kFrontRightDrivingCanId = 4;
+    public static final int kRearRightDrivingCanId = 2;
 
    // CAN IDs for the Extender
     public static final int kExtenderMotor1 = 9;
@@ -27,6 +41,9 @@ public class CanIdConstants {
    // CAN IDs for the Intake
     public static final int kIntakeMotor = 13;
 
+    // CAN IDs for the Intake Rotator
+    public static final int kIntakeRaiseMotor = 17;
+
    // CAN IDs for the Rollers
     public static final int kRollerMotor = 14;
 
@@ -35,5 +52,7 @@ public class CanIdConstants {
 
    // CAN IDs for the Launcher
     public static final int kLauncherMotor = 15;
+
+    
 
 }
